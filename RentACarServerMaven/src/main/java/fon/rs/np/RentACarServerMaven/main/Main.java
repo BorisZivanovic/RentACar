7edf650b37b01181846e135f37a21859cb6aa3eb
BorisZivanovic <borisziv00@gmail.com>
2023-06-28 17:40:57 +0200

    Objasnjeno sta se desava u main metodi na serveru

--- RentACarServerMaven/src/main/java/fon/rs/np/RentACarServerMaven/main/Main.java
+++ RentACarServerMaven/src/main/java/fon/rs/np/RentACarServerMaven/main/Main.java
@@ -7,11 +7,26 @@ package fon.rs.np.RentACarServerMaven.main;
 import fon.rs.np.RentACarServerMaven.forma.ServerskaForma;
 
 /**
- *
- * @author neman
+ * Glavna klasa za pokretanje servera.
+ * 
+ * Klasa Main sadrži statičku metodu main() koja se koristi za pokretanje
+ * serverske forme. Instancira se objekat ServerskaForma i postavlja na vidljiv,
+ * čime se prikazuje prozor serverske forme na ekranu.
+ * 
+ * Ova klasa se koristi za pokretanje servera i prikazivanje korisničkog interfejsa
+ * za upravljanje serverom.
+ * 
+ * @author Boris
  */
+
 public class Main {
 
+	/**
+     * Glavna metoda za pokretanje servera.
+     * 
+     * @param args argumenti komandne linije (ne koriste se)
+     */
+	
     public static void main(String[] args) {
         (new ServerskaForma()).setVisible(true);
     }
