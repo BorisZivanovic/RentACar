9dad5c570c310894de4e67db77db2ec376e522a9
BorisZivanovic <borisziv00@gmail.com>
2023-06-30 14:01:48 +0200

    Mala promena kod testiranja so ObrisiKorisnika

--- RentACarServerMaven/src/test/java/fon/rs/np/RentACarServerMaven/so/korisnik/IzbrisiKorisnikaTest.java
+++ RentACarServerMaven/src/test/java/fon/rs/np/RentACarServerMaven/so/korisnik/IzbrisiKorisnikaTest.java
@@ -2,12 +2,15 @@ package fon.rs.np.RentACarServerMaven.so.korisnik;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import fon.rs.np.RentACarZajednickiMaven.domen.Cenovnik;
 import fon.rs.np.RentACarZajednickiMaven.domen.Korisnik;
+import fon.rs.np.RentACarZajednickiMaven.domen.Mesto;
 import fon.rs.np.RentACarZajednickiMaven.domen.OpstiObjekat;
 import fon.rs.np.RentACarZajednickiMaven.transfer.Odgovor;
 
@@ -16,12 +19,14 @@ class IzbrisiKorisnikaTest {
 	private IzbrisiKorisnika izbrisiKorisnika;
 	private Odgovor odgovor;
 	private OpstiObjekat oo;
+	private Mesto mesto;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		izbrisiKorisnika = new IzbrisiKorisnika();
 		odgovor = new Odgovor();
-		oo = new Korisnik();
+		mesto = new Mesto(1L, "Beograd");
+		oo = new Korisnik(17L, "Marko", "Markovic",234567899L, new Date(3900,11,10), mesto);
 	}
 
 	@AfterEach
