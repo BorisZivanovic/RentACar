d000fd1c061345702638ce27cf46f27bad72c49e
BorisZivanovic <borisziv00@gmail.com>
2023-07-01 13:09:16 +0200

    Napravljena perzistencija liste korisnika u JSON

--- RentACarServerMaven/src/test/java/fon/rs/np/RentACarServerMaven/so/korisnik/VratiKorisnikeTest.java
+++ RentACarServerMaven/src/test/java/fon/rs/np/RentACarServerMaven/so/korisnik/VratiKorisnikeTest.java
@@ -2,9 +2,18 @@ package fon.rs.np.RentACarServerMaven.so.korisnik;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import fon.rs.np.RentACarServerMaven.so.cenovnik.VratiCenovnike;
 import fon.rs.np.RentACarZajednickiMaven.domen.Cenovnik;
@@ -36,7 +45,28 @@ class VratiKorisnikeTest {
 	@Test
 	public void vratiKorisnikeTest() {
 		
-		odgovor = vratiKorisnike.izvrsiTranziciju(oo);
+		try {
+            odgovor = vratiKorisnike.izvrsiTranziciju(oo);
+            
+            if (odgovor.isUspesno()) {
+                List<OpstiObjekat> korisnici = (List<OpstiObjekat>) odgovor.getOdgovor();
+
+                
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+                
+                String jsonString = gson.toJson(korisnici);
+
+                
+                try (FileWriter writer = new FileWriter("src/main/resources/korisnici.json")) {
+                    writer.write(jsonString);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(VratiKorisnike.class.getName()).log(Level.SEVERE, null, ex);
+        }
 		
 		assertTrue(odgovor.isUspesno());
 		assertNotNull(odgovor.getOdgovor());
